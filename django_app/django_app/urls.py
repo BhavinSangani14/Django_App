8af32942c4153b7ea334c6061b786c892d0597bf
Bhavin Sangani <bhavinsangani@192.168.1.9>
2023-02-26 20:45:45 +0530

    templates folder added

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django_app import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("", views.home),
+    path("course/", views.course),
+    path("course/<int:course_id>", views.courses),
+    path("course/<str:course_id>", views.courses),
+    path("course/<slug:course_id>", views.courses),
+
 ]
--- django_app/django_app/views.py
+++ django_app/django_app/views.py
@@ -1,2 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
 
+def home(request):
+    return render(request, "index.html")
      
+def course(request):
+    return HttpResponse("This is a course page")
+
+def courses(request, course_id):
+    return HttpResponse(course_id)
+
+def google(request):
+    return render(request, "index.html")
